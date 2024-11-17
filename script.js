0cbbd5f3b2d1b48a02de23d37ad1dc86266a9345
TheEagleye3 <kayodebusayo113@gmail.com>
2024-11-17 23:14:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {
+  event.target.style.background = "#9e5220";
+  event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("image2");
+
+change2.addEventListener("click", (event) => {
+  event.target.style.background = "#9dc1e2";
+  event.target.style.color = "black";
+});
+
+const change3 = document.getElementById("image3");
+
+change3.addEventListener("click", (event) => {
+  event.target.style.background = "pink";
+  event.target.style.color = "white";
+});
+
+const change4 = document.getElementById("image4");
+
+change4.addEventListener("click", (event) => {
+  event.target.style.background = "#bcafa6";
+  event.target.style.color = "#907079";
+});
+
+const change5 = document.getElementById("image5");
+
+change5.addEventListener("click", (event) => {
+  event.target.style.background = "#3b3a34";
+  event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("image6");
+
+change6.addEventListener("click", (event) => {
+  event.target.style.background = "#53361d";
+  event.target.style.color = "#e4e4de";
+});
+
+const change7 = document.getElementById("image7");
+
+change7.addEventListener("click", (event) => {
+  event.target.style.background = "#786a5b";
+  event.target.style.color = "#cbd4d6";
+});
+
+const change8 = document.getElementById("image8");
+
+change8.addEventListener("click", (event) => {
+  event.target.style.background = "#2f2920";
+  event.target.style.color = "#c5c7cf";
+});
